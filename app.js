35e71c9ce5d1e536917fa71283d927f5cf66340e
Hadi Shanazari <your-email@example.com>
2025-05-02 10:37:48 +0330

    h

--- app.js
+++ app.js
@@ -86,107 +86,36 @@ btnPrev.addEventListener("click", () => {
 
 });
 
+/////////////////////
+//  banerSecond ///
 
-///cube
+const listSecond = document.querySelector(".banerSecond__slider-items");
+const btnNextSecond = document.getElementById("banerSecond__btnNext");
+const btnPrevSecond = document.getElementById("banerSecond__btnPrev");
+const itemsSecond = Array.from(document.querySelectorAll(".banerSecond__slider-item"));
 
-const productsBoxBody = document.querySelectorAll('.products__box-body');
+let positions = [0, 1, 2, 3, 4];
 
+updateClassesSecond();
 
-productsBoxBody.forEach((productsBoxBody) => {
-  const cube = productsBoxBody.querySelector('.cube');
-  const shadow = productsBoxBody.querySelector('.shadowCube');
-  let isDragging = false;
-  let startX, startY;
-  let rotationX = 0, rotationY = 0;
+function updateClassesSecond() {
+  itemsSecond.forEach((item, index) => {
 
-  cube.classList.add('add-rotateY')
-  productsBoxBody.addEventListener('mousedown', (e) => {
-
-    isDragging = !isDragging;
-
-    if (isDragging) {
-      startX = e.clientX;
-      startY = e.clientY;
-    }
-
-
-    if (isDragging) {
-      cube.classList.remove('add-rotateY')
-
-    } else {
-      cube.classList.add('add-rotateY')
-
-    }
+    item.className = "banerSecond__slider-item";
+    item.classList.add(`position-${positions[index]}`);
 
   });
-
-  document.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-
-    let deltaX = e.clientX - startX;
-    let deltaY = e.clientY - startY;
-
-    rotationY += deltaX * 0.5;
-    rotationX -= deltaY * 0.5;
-
-    cube.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
-
-    let shadowOffsetX = rotationY * 0.1;
-    let shadowOffsetY = rotationX * 0.1;
-
-    const shadowX = Math.max(Math.min(shadowOffsetX, 10), -10);
-    const shadowY = Math.max(Math.min(shadowOffsetY, 10), -10);
-
-    shadow.style.transform = `translate(${shadowX}px, ${Math.abs(shadowY)}px)`;
-
-
-    startX = e.clientX;
-    startY = e.clientY;
-  });
-  document.addEventListener('mouseup', (e) => {
-    isDragging = false;
-    cube.classList.add('add-rotateY')
-
-  });
-
-});
-
-//////////////////////scrol
-///////////
-function revealOnScroll() {
-  const box = document.querySelector('.samples_boxs');
-  const boxBodies = document.querySelectorAll('.samples_box-body');
-  const windowHeight = window.innerHeight;
-  const boxTop = box.getBoundingClientRect().top;
-  const revealPoint = 300;
-
-  if (boxTop < windowHeight - revealPoint) {
-    boxBodies.forEach((body, index) => {
-      setTimeout(() => {
-        body.classList.add('scrollActive');
-      }, index * 100);
-    });
-  }
 }
 
-window.addEventListener('scroll', revealOnScroll);
-window.addEventListener('load', revealOnScroll);
+btnNextSecond.addEventListener("click", () => {
+  positions.unshift(positions.pop());
+  updateClassesSecond();
 
-
-
-////////////////////////
-let headerIcon = document.querySelector('.header__menu-icon-toggle');
-let headernav1 = document.querySelector('.header__nav--mobile');
-
-headerIcon.addEventListener("click", () => {
-  headernav1.classList.toggle('active');
 });
 
-
-let activeSubmenu = document.querySelector('.active-submenu');
-let headermenuslide1 = document.querySelector('.header__menu-submenu--mobile');
-
-
-activeSubmenu.addEventListener("click", () => {
-  headermenuslide1.classList.toggle('active');
+btnPrevSecond.addEventListener("click", () => {
+  positions.push(positions.shift());
+  updateClassesSecond();
 });
+
+// END banerSecond ///
